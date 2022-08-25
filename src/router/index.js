7a809d6654f59aa655eab7b435555db6d6a12ca8
Maciej <maciej.preis@newfantastic.com>
2022-08-25 10:51:28 +0200

    add-views

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HelloDonut from '../components/HelloDonut.vue'
-import DonutShop from '../components/DonutShop.vue'
+import Home from '../views/Home.vue'
+import DonutShop from '../views/DonutShop.vue'
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: HelloDonut },
+        { path: '/', component: Home },
         { path: '/DonutShop', component: DonutShop }
     ]
 })
