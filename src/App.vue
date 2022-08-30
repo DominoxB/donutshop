65448ad593ffb862280ade2de63f7b78cfb7f27e
Dominika <dominika-biedulska@tlen.pl>
2022-08-30 12:35:57 +0200

    add router view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <Navbar></Navbar>
   <router-view></router-view>
   <!-- <router-link to="/DonutShop"></router-link> -->
 </template>
 
 <script>
+import Navbar from './components/molecules/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-  }
+    Navbar,
+}
 }
 </script>
---
+++ src/components/molecules/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-screen h-screen p-4 pb-6">
+    <h1 class="pb-8 text-5xl text-center font-bold text-indigo-800">
+      Welcome in our Donuts Shop
+      <h1 class="text-amber-800 pt-2">🍩DONUTINO🍩</h1>
+    </h1>
+    <div class="flex items-center justify-center space-x-16">
+      <CustomLink name="Home" url="/"></CustomLink>
+      <CustomLink name="Shop" url="/DonutShop"></CustomLink>
+      <CustomLink name="About Donuts" url="/AboutDonuts"></CustomLink>
+      <CustomLink name="Donuts World" url="/DonutsWorld"></CustomLink>
+      <CustomLink name="About Us" url="/AboutUs"></CustomLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import CustomLink from "../atoms/CustomLink.vue";
+export default {
+  name: "MyNavbar",
+  components: {
+    CustomLink,
+},
+};
+</script>
