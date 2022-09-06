4cfb0deebee50453072d8943597b95bc24ab6081
Dominika <dominika-biedulska@tlen.pl>
2022-09-06 14:04:52 +0200

    centering subpages

--- src/components/atoms/DonutItem.vue
+++ src/components/atoms/DonutItem.vue
@@ -3,7 +3,7 @@
   <div
     class="grid items-center justify-center group transition ease-in-out delay-100 group-hover:-translate-y-1 hover:scale-110 cursor-pointer"
   >
-    <img :src="donut.image" class="w-64 h-72 object-contain">
+    <img :src="donut.image" class="w-64 h-72 object-contain"/>
     <div
       class="text-xl text-center text-blue-900 font-medium group-hover:text-blue-500"
     >
