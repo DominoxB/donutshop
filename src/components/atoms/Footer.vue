abe2272db15aaaa5caf98a9d32af225332ab4139
Dominika <dominika-biedulska@tlen.pl>
2022-09-08 17:35:31 +0200

    add footer

---
+++ src/components/atoms/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+        <div class="bg-gray-400 relative bottom-0 left-0 right-0 h-52 mt-10">
+            <div class="block text-xl text-purple-800 text-center py-2">DONUTINO - nasze wypieki tworzymy z pasją już od 30 lat!</div>
+            <div class="justify-start items-center pl-4">Donata Pączkowska<br>
+            ul. Czekoladowa 17<br>
+            88-888 Pączkowice Wielkie</div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterSection',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container mx-auto">
     <p>{{ $t("message.header") }}</p>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+  import Footer from '../components/atoms/Footer.vue'
 export default {
-  name: "HomePage",
+    name: "HomePage",
+    components: {
+      Footer
+    }
 };
 </script>
