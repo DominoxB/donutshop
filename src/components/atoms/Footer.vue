8e32ca0d767c9c8b5be96c619052145a3c2ef4ea
Dominika <dominika-biedulska@tlen.pl>
2022-09-14 12:55:09 +0200

    add socials to footer

--- src/components/atoms/Footer.vue
+++ src/components/atoms/Footer.vue
@@ -1,15 +1,21 @@
 <template>
   <footer>
     <div class="bg-gray-200 border-t-zinc-400 border-t-2 h-46 mt-10 items-end">
-      <div class="block text-base font-semibold pl-4 py-2">
-        🍩DONUTINO🍩 - nasze wypieki tworzymy z pasją już od 30 lat!
+      <div class="flex text-base font-semibold pl-4 py-2 justify-between">
+        <span>🍩DONUTINO🍩 - nasze wypieki tworzymy z pasją już od 30 lat!</span>
+        <div class="flex">
+          <span>Szukaj nas na: </span>
+          <img src="@/images/fb.png" class="w-7 h-7 mt-2 ml-3"/>
+          <img src="@/images/insta.png" class="w-8 h-8 ml-3 mt-2"/>
+          <img src="@/images/tik.png" class="w-7 h-7 ml-3 mt-2 mr-3"/>
+        </div>
       </div>
       <div class="text-center text-sm font-thin font-serif">
         <p>Donata Pączkowska</p>
         <p>ul. Czekoladowa 17</p>
         <p>88-888 Pączkosławice Wielkie</p>
       </div>
-      <div class="text-end font-medium">&copy; 2022 Domino IT Polska</div>
+      <div class="text-end font-medium mr-3">&copy; 2022 Domino IT Polska</div>
     </div>
   </footer>
 </template>
--- src/components/atoms/HomeContent.vue
+++ src/components/atoms/HomeContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto min-h-[500px] mt-6">
     <div class="bg-slate-200 grid grid-cols-1 md:grid md:grid-cols-2 lg:flex lg:items-center lg:justify-center">
-    <p class="text-2xl font-light text-zinc-800 text-center leading-loose pl-3 pr-3">{{ $t("message.header") }} </p>
+    <p class="text-2xl font-light text-zinc-800 text-center leading-loose pl-4 pr-4">{{ $t("message.header") }} </p>
     <img src="@/images/don.jpg" class= "md:h-[624px] object-fill"/>
   </div>
   </div>
