ce750e69ebfd133b394f22ba1647624a377848b0
Dominika <dominika-biedulska@tlen.pl>
2022-09-15 13:28:09 +0200

    add footer to contact page

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <FormContent></FormContent>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import FormContent from '@/components/atoms/FormContent.vue';
+import Footer from '@/components/atoms/Footer.vue';
 export default {
     name: "ContactPage",
-    components: { FormContent }
+    components: { FormContent, Footer }
 }
 </script>
