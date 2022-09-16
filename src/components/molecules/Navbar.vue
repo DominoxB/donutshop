1b9f590a3303b9c4d02f5ad79ae6d6fc41204aed
Dominika <dominika-biedulska@tlen.pl>
2022-09-16 10:15:17 +0200

    add responsive logo

--- src/components/molecules/Navbar.vue
+++ src/components/molecules/Navbar.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="bg-white w-screen pt-4 z-10 md:relative lg:relative">
     <div
-      class="flex md:block justify-between text-2xl md:text-4xl md:text-center md:pb-4 lg:text-6xl lg:pb-4 text-amber-600"
+      class="flex justify-between items-center text-2xl md:text-4xl md:block md:text-center md:pb-4 lg:text-6xl lg:pb-4 text-amber-600"
     >
       <div>
+        <span class="font-pacifico visible md:invisible ml-4">DONUTINO</span>
+      </div>
+
+      <div>
         <router-link to="/">
-          <img src="@/images/donutino.png" class="invisible md:visible w-24 h-24 mx-auto pb-2" />
-          <span class="font-pacifico">DONUTINO</span>
+          <img
+            src="@/images/donutino.png"
+            class="invisible md:visible w-24 h-24 mx-auto pb-2"
+          />
+          <span class="font-pacifico invisible md:visible">DONUTINO</span>
         </router-link>
       </div>
       <NavbarHamburger
-          @mobileMenu="showMenu($event)"
-          class="flex my-auto md:hidden mr-14"
-        ></NavbarHamburger>
-
+        @mobileMenu="showMenu($event)"
+        class="flex my-auto md:hidden"
+      ></NavbarHamburger>
     </div>
-
   </div>
   <div
     class="sticky hidden font-pacifico md:block lg:block top-0 bg-white text-l text-center py-2 md:text-2xl lg:text-4xl items-center justify-center space-x-8"
