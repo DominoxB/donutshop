2423e7340bb038e7df83ed5aa7a56faf73c88185
Dominika <dominika-biedulska@tlen.pl>
2022-09-22 13:29:17 +0200

    change duration in hamburger

--- src/components/atoms/NavbarHamburger.vue
+++ src/components/atoms/NavbarHamburger.vue
@@ -3,7 +3,7 @@
     <button @click="clickHamburger" class="relative group">
       <div class="relative flex overflow-hidden items-center justify-center">
         <div
-          class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden"
+          class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all origin-center overflow-hidden"
         >
           <div
             :class="{ 'translate-x-10': show }"
@@ -19,15 +19,15 @@
           ></div>
 
           <div
-            class="absolute items-center justify-between transform transition-all duration-500 top-2.5 flex"
+            class="absolute items-center justify-between transform transition-all top-2.5 flex"
           >
             <div
               :class="{ 'rotate-45': show }"
-              class="absolute bg-zinc-800 h-[2px] w-5 transform transition-all duration-300 delay-100"
+              class="absolute bg-zinc-800 h-[2px] w-5 transform transition-all duration-200 delay-100"
             ></div>
             <div
               :class="{ '-rotate-45': show }"
-              class="absolute bg-zinc-800 h-[2px] w-5 transform transition-all duration-300 delay-100"
+              class="absolute bg-zinc-800 h-[2px] w-5 transform transition-all duration-200 delay-100"
             ></div>
           </div>
         </div>
