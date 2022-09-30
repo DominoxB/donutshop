7e49eb605c3cdd53c6d96084303e4529840b5b65
Dominika <dominika-biedulska@tlen.pl>
2022-09-30 11:41:58 +0200

    add buttons to basket page

--- src/components/atoms/BasketContent.vue
+++ src/components/atoms/BasketContent.vue
@@ -1,9 +1,27 @@
 <template>
-    <div class="container mx-auto min-h-[500px]"></div>
+  <div class="container mx-auto min-h-[500px]">
+    <div class="block">
+      <div class="flex"></div>
+    </div>
+    <div class="flex justify-between">
+      <router-link to="/DonutShop">
+        <button
+          class="border-4 border-amber-800 hover:bg-amber-200 rounded p-3 font-pacifico text-2xl"
+        >
+          Kontunuuj zakupy
+        </button>
+      </router-link>
+      <button
+        class="border-4 border-amber-800 hover:bg-amber-200 rounded p-3 font-pacifico text-2xl"
+      >
+        Przejdź do kasy
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "BasketContent"
-}
+  name: "BasketContent",
+};
 </script>
