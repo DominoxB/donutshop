b086349da1c66f675a4e0ce884227529a07ea39f
Dominika <dominika-biedulska@tlen.pl>
2022-10-06 10:23:12 +0200

    change button width and heigh

--- src/components/atoms/BtnScrollToTop.vue
+++ src/components/atoms/BtnScrollToTop.vue
@@ -1,9 +1,10 @@
 <template>
   <button
+    v-show="isVisible"
     @click="scroll"
-    class="fixed z-10 bottom-8 right-10 border-0 w-16 h-16 rounded-full drop-shadow-md bg-indigo-300 hover:bg-indigo-400 text-white text-3xl font-bold"
+    class="fixed z-10 bottom-8 right-2 border-0 w-12 h-12 rounded-full drop-shadow-md bg-indigo-300 hover:bg-indigo-400 text-white text-3xl font-bold scroll-smooth"
   >
-    &#129045;
+    &#129045; 
   </button>
 </template>
 
@@ -11,11 +12,16 @@
 export default {
   name: "BtnScrollToTop",
   setup() {
+    const isVisible = () => {
+      window.scrollY > 150
+    }
     const scroll = () => {
       window.scrollTo(0, 0);
-    };
+    }
+    console.log(window)
     return {
       scroll,
+      isVisible,
     };
   },
 };
