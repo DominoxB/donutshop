b497f6817d1607dcfe01ea8cf39672f988b98554
Dominika <dominika-biedulska@tlen.pl>
2022-10-10 13:29:39 +0200

    add footer

--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,11 +1,13 @@
 <template>
   <PaymentUserData></PaymentUserData>
+  <Footer></Footer>
 </template>
 
 <script>
 import PaymentUserData from '@/components/atoms/PaymentUserData.vue';
+import Footer from '@/components/atoms/Footer.vue';
 export default {
     name: "PaymentPage",
-    components: { PaymentUserData }
+    components: { PaymentUserData, Footer }
 }
 </script>
