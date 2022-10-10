d2333259d09abf83633b6a2fbd509e07749395ce
Dominika <dominika-biedulska@tlen.pl>
2022-10-10 13:30:23 +0200

    add btn scroll to top

--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,13 +1,15 @@
 <template>
   <PaymentUserData></PaymentUserData>
+  <BtnScrollToTop></BtnScrollToTop>
   <Footer></Footer>
 </template>
 
 <script>
 import PaymentUserData from '@/components/atoms/PaymentUserData.vue';
 import Footer from '@/components/atoms/Footer.vue';
+import BtnScrollToTop from '@/components/atoms/BtnScrollToTop.vue';
 export default {
     name: "PaymentPage",
-    components: { PaymentUserData, Footer }
+    components: { PaymentUserData, Footer, BtnScrollToTop }
 }
 </script>
