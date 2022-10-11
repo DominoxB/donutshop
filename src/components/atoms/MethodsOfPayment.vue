28779d44b62a6982b0e80868c540b04ed344bd80
Dominika <dominika-biedulska@tlen.pl>
2022-10-11 12:27:30 +0200

    add delivery

--- src/components/atoms/MethodsOfPayment.vue
+++ src/components/atoms/MethodsOfPayment.vue
@@ -1,31 +1,51 @@
 <template>
   <div class="bg-slate-200 font-garamond border border-black rounded block mt-12 ml-auto mr-auto p-4">
+    <!-- SPOSÓB PŁATNOŚCI -->
     <p class="text-xl font-semibold mb-2">Wybierz metodę płatności: </p>
     <div class="flex text-lg mb-2">
-      <input type="radio" value="gotówka" id="cash" name="chosen" v-model="chosen"/>
+      <input type="radio" value="gotówka" id="cash" name="selected" v-model="selected"/>
       <label>Gotówka przy odbiorze</label>
       <img src="@/images/cash.png" class="w-10 h-8 ml-2"/>
     </div>
     <div class="flex text-lg mb-2">
-      <input type="radio" value="karta płatnicza" id="card" name="chosen" v-model="chosen"/>
+      <input type="radio" value="karta płatnicza" id="card" name="selected" v-model="selected"/>
       <label>Karta płatnicza</label>
       <img src="@/images/karta.png" class="w-20 h-6 ml-2"/>
     </div>
     <div class="flex text-lg mb-2">
-      <input type="radio" value="przelew internetowy" id="transfer" name="chosen" v-model="chosen"/>
+      <input type="radio" value="przelew internetowy" id="transfer" name="selected" v-model="selected"/>
       <label>Przelewy24 (szybki przelew online)</label>
       <img src="@/images/przelewy.png" class="w-12 h-6 ml-2"/>
     </div>
     <div class="flex text-lg mb-2">
-      <input type="radio" value="blik" id="blik" name="chosen" v-model="chosen"/>
+      <input type="radio" value="blik" id="blik" name="selected" v-model="selected"/>
       <label>Blik</label>
       <img src="@/images/blik.png" class="w-8 h-8 ml-2"/>
     </div>
-    <div>
-      <h1>Wybrany sposób płatności: {{ chosen }}</h1>
+    <div class="mt-2 border-t-2 bg-slate-300">
+      <h1 class="p-2">Wybrany sposób płatności: <span class="font-semibold">{{ selected }}</span></h1>
+    </div>
+    <!-- DOSTAWA -->
+    <div class="mt-4 border-t-2 border-black">
+    <p class="text-xl font-semibold mt-2">Wybierz sposób dostawy: </p>
+    <div class="flex text-lg mb-2">
+      <input type="radio" value="kurier DPD" id="courier" name="delivery" v-model="delivery"/>
+      <label>Kurier DPD (12,99)</label>
+    </div>
+    <div class="flex text-lg mb-2">
+      <input type="radio" value="paczkomat InPost Fresh" id="inpost" name="delivery" v-model="delivery"/>
+      <label>Paczkomat InPost Fresh (9,99)</label>
+    </div>
+    <div class="flex text-lg mb-2">
+      <input type="radio" value="odbiór osobisty" id="self" name="delivery" v-model="delivery"/>
+      <label>Odbiór osobisty w Pączkowicach (0,00)</label>
+    </div>
+    <div class="mt-2 border-t-2 bg-slate-300">
+      <h1 class="p-2">Wybrany sposób dostawy: <span class="font-semibold">{{ delivery }}</span></h1>
     </div>
 
 
+  </div>
   </div>
 </template>
 
@@ -34,9 +54,11 @@ import { ref } from 'vue'
 export default {
   name: "MethodsOfPayment",
   setup() {
-    const chosen = ref(null)
+    const selected = ref(null)
+    const delivery = ref(null)
     return {
-      chosen
+      selected,
+      delivery
     }
   }
 
