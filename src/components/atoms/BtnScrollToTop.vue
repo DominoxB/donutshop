490373a56a8c31d44ef635ec1edc69585ba95f33
Dominika <dominika-biedulska@tlen.pl>
2022-10-12 12:06:37 +0200

    add style to scroll btn

--- src/components/atoms/BtnScrollToTop.vue
+++ src/components/atoms/BtnScrollToTop.vue
@@ -2,7 +2,7 @@
   <button
     v-show="scY > 150"
     @click="scroll"
-    class="fixed z-10 bottom-8 right-2 border-0 w-12 h-12 rounded-full drop-shadow-md bg-indigo-300 hover:bg-indigo-400 text-white text-3xl font-bold scroll-smooth"
+    class="fixed z-10 bottom-8 right-2 border-0 w-12 h-12 rounded-full drop-shadow-md bg-indigo-300 opacity-30 hover:bg-indigo-400 text-white text-3xl font-bold scroll-smooth"
   >
     &#129045;
   </button>
