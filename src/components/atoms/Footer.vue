ba0f3e099503e16d3d34838e039f4485a6fcc02d
Dominika <dominika-biedulska@tlen.pl>
2022-10-19 14:37:38 +0200

    change in footer and btn scroll

--- src/components/atoms/Footer.vue
+++ src/components/atoms/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="grid grid-cols-1 bg-gray-100 border-t-zinc-400 border-t-2 h-46 mt-10 items-end font-thin text-slate-800">
-      <div class="flex text-base font-pacifico justify-center mt-3">
+      <div class="flex text-base font-pacifico justify-center text-center mt-3 px-2">
         <span>DONUTINO - nasze wypieki tworzymy z pasją już od 30 lat!</span>
       </div>
       <div class="text-center font-garamond text-sm mt-2">
@@ -21,7 +21,7 @@
             <img src="@/images/tik.png" class="w-5 h-5 md:w-7 md:h-7 ml-3 mt-2 mr-3" />
           </a>
         </div>
-      <div class="text-end text-sm md:text-lg mr-3 font-garamond mt-2">&copy; 2022 Domino IT Polska</div>
+      <div class="text-end text-sm md:text-lg mr-3 font-garamond mt-3">&copy; 2022 Domino IT Polska</div>
     </div>
   </footer>
 </template>
