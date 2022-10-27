122e1dbe28ce87540da5d5dd827b829d031c0e99
Dominika <dominika-biedulska@tlen.pl>
2022-10-27 11:11:56 +0200

    add publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/Home" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/donutshop/" : "/",
   transpileDependencies: true
 })
